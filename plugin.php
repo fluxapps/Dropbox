3b333476762e961b1f4c0b217190707c38b538a6
Fabian Schmid <fs@studer-raimann.ch>
2015-09-23 08:08:11 +0200

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -3,6 +3,6 @@ $id = "cdpx";
 $version = "1.0.0";
 $ilias_min_version = "4.4.0";
 $ilias_max_version = '5.1.999';
-$responsible = "Timon Amstutz";
-$responsible_mail = "timon.amstutz@ilub.unibe.ch";
+$responsible = "studer + raimann ag";
+$responsible_mail = "support@studer-raiman.ch";
 ?>
